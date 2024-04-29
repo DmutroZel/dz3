b6a1b6a7b95b054604e3895200d5310b113bb31a
DmutroZel <156704336+DmutroZel@users.noreply.github.com>
2024-04-29 17:01:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+box.onclick = function(){
+    box.style.bottom = "30px";
+}
+box2.onclick = function(){
+    box2.style.bottom = "30px";
+}
+
+box3.onclick = function(){
+    box3.style.bottom = "30px";
+}
+
+box4.onclick = function(){
+    box4.style.bottom = "30px";
+}
+
